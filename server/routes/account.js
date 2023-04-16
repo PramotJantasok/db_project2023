10d02598f899a612da0a3beef5e777ef1e09d177
Wichai <wichailoveyou3000@gmail.com>
2023-04-16 21:39:38 +0700

    BIG

--- server/routes/account.js
+++ server/routes/account.js
@@ -6,14 +6,21 @@ router = express.Router();
 
 router.post('/register',async (req,res,next) => {
     try{
-        const {user_name,user_pass,user_fname,user_lname} = req.body;
+        const {user_name,user_pass,user_fname,user_lname,user_gender,user_plan,user_age,user_weight,user_height} = req.body;
 
-        
         console.log(req.body);
-        // const [row,fields] = await conn.query(
-        //     "INSERT INTO `USER` (user_name,user_password,user_fname,user_lname) values (?,?,?,?)",
-        //     [user_name,user_pass,user_fname,user_lname]
-        // )
+        const [row,fields] = await conn.query(
+            "INSERT INTO USER (user_name,user_password,user_fname,user_lname) values (?,?,?,?)",
+            [user_name,user_pass,user_fname,user_lname]
+        )
+        const [row2,fields2] = await conn.query(
+            "INSERT INTO PLAN (plan_name) values (?)",
+            [user_plan]
+        )
+        const [row3,fields3] = await conn.query(
+            "INSERT INTO PROFILE (user_id,profile_age,profile_height,profile_weight,profile_gender) values (?,?,?,?,?)",
+            [row.insertId,user_age,user_height,user_weight,user_gender]
+        )
         res.render("login")
     }catch(er){
         console.log(er);
