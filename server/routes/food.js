ef797a5fe99b8889d93e73e210471dec68ce50a8
Wichai <wichailoveyou3000@gmail.com>
2023-04-16 23:42:06 +0700

    BIG

--- server/routes/food.js
+++ server/routes/food.js
@@ -3,7 +3,7 @@ const conn = require('../configs/config_aws');
 
 router = express.Router();
 
-function dataTime(){
+function dataTime() {
     const today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth() + 1;
@@ -14,23 +14,55 @@ function dataTime(){
     if (mm < 10) {
         mm = '0' + mm;
     }
-    console.log(yyyy+'-'+mm+'-'+dd)
-    return yyyy+'-'+mm+'-'+dd;
+    console.log(yyyy + '-' + mm + '-' + dd)
+    return yyyy + '-' + mm + '-' + dd;
 }
 
-router.get('/table/food', async (req, res, next) =>{
+router.get('/table/food', async (req, res, next) => {
     const time = dataTime();
-    const [row, field] = await conn.query('select * from DAILY where user_id = ?, daily_date = ?',[
-        1, time
+    const id = req.get("Cookie").split('token=')[1].trim();
+    const [row, field] = await conn.query('select * from DAILY where user_id = ?, daily_date = ?', [
+        id, time
     ])
-    res.render('tableFood.ejs', {data: row[0]});
+
+    if (row.length == 0) {
+        const create = await conn.query('insert into DAILY(daily_date, user_id) valuse(?,?)', [
+            time, id
+        ])
+        console.log(create.insertId)
+        // const [newDialy, field] = await conn.query('select * from DAILY where user_id = ?, daily_date = ?',[
+        //     id, time
+        // ]);
+
+        const [daily, filed2] = await conn.query('select * from MEAL left outer join MEAL_FOOD using(meal_id) where daily_id = ?', [
+            create.insertId
+        ]);
+        console.log(daily);
+        res.render('tableFood', {
+            dialy: daily[0]
+        });
+
+    } else {
+        const [Dialy, field] = await conn.query('select * from DAILY where user_id = ?, daily_date = ?',[
+            id, time
+        ]);
+        const [dailys, filed2] = await conn.query('select * from MEAL left outer join MEAL_FOOD using(meal_id) where daily_id = ?', [
+            Dialy[0].daily_id
+        ]);
+        console.log(dailys);
+        res.render('tableFood', {
+            dialy: dailys[0]
+        });
+    }
+
 });
 
-router.get('/table/food/history'), async (req, res, next) =>{
-    
+router.get('/table/food/history'), async (req, res, next) => {
+
     const [row, field] = await conn.query('select * from DAILY where user_id = ?');
     const [row2, filed2] = await conn.query('select * from MEAL_FOOD where user_id = ?')
     const [row3, filed3] = await conn.query('select * from food');
+
     res.render('')
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const conn = require('../configs/config_aws');
 
-
-
 router = express.Router();
 
 router.get('/', (req, res, next)=>{
@@ -19,20 +17,29 @@ router.get('/register',(req,res,next) => {
 
 router.get('/login',(req,res,next) =>{
     res.render("login");
-})
+});
 
 router.get('/food', (req,res,next) => {
     res.render('food');
-})
+});
+
  router.get('/addfood' , (req,res,next) => {
     res.render('addfood');
- })
+ });
 
  router.get('/profile' , (req,res,next) => {
     res.render('profile')
- })
+ });
 
-router.get('/tableFood', (req,res,next) => {
+router.get('/tableFood', async (req,res,next) => {
+    try{
+        const [row, field] = await conn.query('select * ')
+    }catch(er){
+        console.log(er);
+        res.status(404);
+    }
     res.render('tableFood');
-})
+});
+
+
 exports.router = router;
