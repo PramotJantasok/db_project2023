17a521b539e73f3b4eae1d9152b55ce30435831d
Anaphat Nuntanapiboon <88420410+anaphat64070249@users.noreply.github.com>
2023-04-16 22:20:00 +0700

    frame

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const conn = require('../configs/config_aws');
+
+
+
 router = express.Router();
 
 router.get('/', (req, res, next)=>{
@@ -18,4 +21,18 @@ router.get('/login',(req,res,next) =>{
     res.render("login");
 })
 
+router.get('/food', (req,res,next) => {
+    res.render('food');
+})
+ router.get('/addfood' , (req,res,next) => {
+    res.render('addfood');
+ })
+
+ router.get('/profile' , (req,res,next) => {
+    res.render('profile')
+ })
+
+router.get('/tableFood', (req,res,next) => {
+    res.render('tableFood');
+})
 exports.router = router;
